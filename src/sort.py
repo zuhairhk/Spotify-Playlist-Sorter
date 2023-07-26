36564fd04760045757d38840491988bc7f35a428
zuhairhk <zuhairhk@gmail.com>
2023-07-26 14:24:18 -0400

    Playlist creation update

--- src/sort.py
+++
@@ -1,2 +0,0 @@
-from spot import genreList
-
--- src/sorter.py
+++ src/sorter.py
@@ -1,9 +1,14 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+import os
 
 file = open("out.txt","w+")
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+                                               client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
+                                               redirect_uri=os.getenv('SPOTIPY_REDIRECT_URI'),
+                                               scope="playlist-modify-public"))
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
 
 def get_track_genres(track_id):
     try:
@@ -16,6 +21,7 @@ def get_track_genres(track_id):
     
 
 def organize_playlist_by_genre(playlist_id):
+    i = 1
     tracks_by_genre = {}
     playlist_tracks = sp.playlist_tracks(playlist_id)
 
@@ -28,7 +34,8 @@ def organize_playlist_by_genre(playlist_id):
                 track_id = '0tMMPZEt6Gyrl9FI8zSicm'
                 track_name = 'Glue Song'
             
-            print(track_name)
+            print(i, ': ', track_name)
+            i += 1
             
             track_genres = get_track_genres(track_id)
             
@@ -42,13 +49,29 @@ def organize_playlist_by_genre(playlist_id):
     file.write(str(tracks_by_genre))
     print(tracks_by_genre)
 
-    '''
-    # create new playlist for each genre and add tracks to playlist
+    #updated_tbr = {}
+    updated_tbr = select_playlists(tracks_by_genre)
+    create_playlists(updated_tbr)
+
+def select_playlists(tracks_by_genre):
+    for genre in tracks_by_genre:
+        print(genre)
+    selected_genre = input('Enter genre to make a playlist off: ')
+
+    new_tbr = {}
+    if selected_genre in tracks_by_genre:
+        new_tbr[selected_genre] = tracks_by_genre[selected_genre]
+
+    return new_tbr
+
+
+def create_playlists(tracks_by_genre):
+    # create new playlist for a specific genre and add tracks to that playlist
     for genre in tracks_by_genre:
         # create new playlist with genre name
         playlist_name = f"{genre} Playlist"
-        sp.user_playlist_create(user='', name=playlist_name)
-        # retrieve playlist ID for newly created playlist
+        sp.user_playlist_create(user='zuhairhk', name=playlist_name)
+        # retrieve playlist ID for the newly created playlist
         playlist_id = sp.current_user_playlists()['items'][0]['id']
         # add tracks to playlist
         track_ids = []
@@ -57,9 +80,8 @@ def organize_playlist_by_genre(playlist_id):
             if results['tracks']['items']:
                 track_uri = results['tracks']['items'][0]['uri']
                 track_ids.append(track_uri)
-        #sp.playlist_add_items(playlist_id, track_ids)
+        sp.playlist_add_items(playlist_id, track_ids)
         print(playlist_id, track_ids)
-    '''
 
 
-organize_playlist_by_genre('6PfP2dLuEjryzCK9Fw4b1M') #YEAH=26jDYsxAgRqpOIRovfWU9L, smtiforgot=2yD67LQ7HpqLKUQKR5JTet
+organize_playlist_by_genre('6PfP2dLuEjryzCK9Fw4b1M') #YEAH=26jDYsxAgRqpOIRovfWU9L, smtiforgot=2yD67LQ7HpqLKUQKR5JTet '03JS3MM4SVhnODKMJOV5Mt'
